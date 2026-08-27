a554790b78b7322ac5b10ace332b9326eb2f2ebc
Yugo25 <mugiwara_yugo@icloud.com>
2026-08-27 12:47:59 -0400

    Use forEach

--- js/story.js
+++ js/story.js
@@ -4,9 +4,9 @@ Story
 const mainImage = document.querySelector('.story-image img');
 const thumbImages = document.querySelectorAll('.story-thumbnails img');
 
-for (let i = 0; i < thumbImages.length; i++) {
-    thumbImages[i].addEventListener('mouseover', (event) => {
+thumbImages.forEach((thumbImage)=>{
+    thumbImage.addEventListener('mouseover', (event) => {
         mainImage.src = event.target.src;
         mainImage.animate({opacity: [0, 1]}, 500);
     });
-}
+});
