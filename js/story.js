c6e7feaadd1eed665bda957928eef2f6baf0b358
Yugo25 <mugiwara_yugo@icloud.com>
2026-08-27 12:43:28 -0400

    The big pic can be the cursor

--- js/story.js
+++ js/story.js
@@ -5,7 +5,8 @@ const mainImage = document.querySelector('.story-image img');
 const thumbImages = document.querySelectorAll('.story-thumbnails img');
 
 for (let i = 0; i < thumbImages.length; i++) {
-    thumbImages[i].addEventListener('mouseover', () => {
+    thumbImages[i].addEventListener('mouseover', (event) => {
+        mainImage.src = event.target.src;
         mainImage.animate({opacity: [0, 1]}, 500);
     });
 }
