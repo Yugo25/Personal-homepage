a50ab351dc514ffff91099b85ab77f419981d69a
Yugo25 <mugiwara_yugo@icloud.com>
2026-08-28 10:36:57 -0400

    Slide menu completed

--- js/slideMenu.js
+++ js/slideMenu.js
@@ -1,6 +1,7 @@
 const menuOpen = document.querySelector('#menu-open');
 const menuClose = document.querySelector('#menu-close');
 const menuPanel = document.querySelector('#menu-panel');
+const menuItems = document.querySelectorAll('#menu-panel li');
 const menuOptions = {
     duration: 1400,
     easing: 'ease',
@@ -10,9 +11,28 @@ const menuOptions = {
 // Open the menu
 menuOpen.addEventListener('click', () => {
     menuPanel.animate({translate: ['100vw', 0]}, menuOptions);
+
+    // Show the items one by one
+    menuItems.forEach((menuItem, index) => {
+        menuItem.animate(
+            {
+                opacity: [0, 1],
+                translate: ['2rem', 0],
+            },
+            {
+                duration: 2400,
+                delay: 300 * index,
+                easing: 'ease',
+                fill: 'forwards',
+            }
+        );
+    });
 });
 
 // Close the menu
 menuClose.addEventListener('click', () => {
     menuPanel.animate({translate: [0, '100vw']}, menuOptions);
+    menuItems.forEach((menuItem) => {
+        menuItem.animate({opacity: [1, 0]}, menuOptions);
+    });
 });
